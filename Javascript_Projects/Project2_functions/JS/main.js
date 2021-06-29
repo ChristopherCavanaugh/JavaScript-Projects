b10acbc454720dd28a4bfb7ff533c3c60ae4241e
ChristopherCavanaugh <85709155+ChristopherCavanaugh@users.noreply.github.com>
2021-06-29 12:21:17 -0700

    Add files via upload

---
+++ Javascript_Projects/Project2_functions/JS/main.js
@@ -0,0 +1,11 @@
+function My_First_Function() {
+    var str = "This is my pink text";
+    var resut = str.fontcolor("pink");
+    document.getElementById("pink_text").innerHTML = result;
+}
+
+function myFunction() { //Function using += operator
+    var sentence = "I am learning";
+    sentence += " a lot from this book!";
+    document.getElementById("Concatenate").innerHTML = sentence;
+}
