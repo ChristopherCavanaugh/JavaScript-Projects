5926533b71a4262d3f062159fbc7a05dbb26f9a8
ChristopherCavanaugh <85709155+ChristopherCavanaugh@users.noreply.github.com>
2021-06-29 11:56:10 -0700

    Add files via upload

---
+++ Javascript_Projects/Project1_expressions_alert/JS/main.js
@@ -0,0 +1,14 @@
+var A = "this is a string"; // assigning a string variable
+document.write(A);
+window.alert("dice"); //window pop up with assigned varriable B
+var C = " Concantenated " + " string " 
+document.write(C) // concaneting a string
+
+var family = "the cavanaughs", Dad ="chris", son ="milton"; //assigning a variable family
+document.write(Dad);
+
+var blues = "i have the blues."; //assigning variable and assigning a variable with colors
+var blues = blues.fontcolor("blue");
+document.write(blues);
+
+document.write(3+3); //adding 3 plus 3
