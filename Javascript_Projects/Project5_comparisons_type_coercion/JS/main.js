3bda8518a03bc8528103d0ee6becd0c2b878aa72
ChristopherCavanaugh <85709155+ChristopherCavanaugh@users.noreply.github.com>
2021-06-30 11:57:13 -0700

    java fixes

---
+++ Javascript_Projects/Project5_comparisons_type_coercion/JS/main.js
@@ -0,0 +1,13 @@
+document.write(typeof "word");
+
+document.write(typeof 3);
+
+document.write("10" +5);
+
+document.write(2E310);
+
+document.write(-3E310);
+
+document.write(10>2);
+
+document.write(10<2);
